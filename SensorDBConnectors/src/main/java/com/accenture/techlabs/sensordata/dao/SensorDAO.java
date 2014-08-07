b2ab4198c073ad2bfbe5288a412fa2a29311f0d8
ktaneja <taneja.kunal@gmail.com>
2014-08-06 18:46:25 -0700

    Separated Code for data access

--- SensorDBConnectors/src/main/java/com/accenture/techlabs/sensordata/dao/SensorDAO.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.techlabs.sensordata.dao;
-
-import java.util.Calendar;
-
-public interface SensorDAO {
-	public boolean createTable(String deviceID, SensorDataType datatype);
-	public boolean addData(String deviceID, SensorObservationData data);
-	public SensorObservationData getData(String deviceID, Calendar from, Calendar to);
-}
--- SensorDBConnectors/src/main/java/com/accenture/techlabs/sensordata/dao/SensorObservationData.java
+++
@@ -1,9 +0,0 @@
-package com.accenture.techlabs.sensordata.dao;
-
-import java.util.Vector;
-
-public class SensorObservationData {
-	String sensorID;
-	SensorDataType dataType;
-	Vector<Object> data;
-}
